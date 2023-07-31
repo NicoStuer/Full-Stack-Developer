73d5cf11f6d4b8e22bbf8cb02fb02fce464c1e6a
“Nicolas <nicolas.m.stuer@gmail.com>
2023-07-31 19:28:46 -0300

    Cambios configuracion entrega-final-segundo-cuatromestre

--- Full-Stack-Developer-Career/Entregas/Shopping-Cart-PW/vite.config.js
+++ Full-Stack-Developer-Career/Entregas/Shopping-Cart-PW/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Full-Stack-Developer-Career/Entregas/Shopping-Cart-PW/'
 })
